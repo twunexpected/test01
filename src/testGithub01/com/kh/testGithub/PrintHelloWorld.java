50f0d3a548db3778d3ac9d2f155210f3349f2db4
user1 <user1@KH_C>
2019-11-12 14:48:06 +0900

    마스터가 추가했음

--- src/testGithub01/com/kh/testGithub/PrintHelloWorld.java
+++ src/testGithub01/com/kh/testGithub/PrintHelloWorld.java
@@ -5,5 +5,7 @@ public class PrintHelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		
+		System.out.println("New massage added !!");
+		System.out.println("New added");
 	}
 }
