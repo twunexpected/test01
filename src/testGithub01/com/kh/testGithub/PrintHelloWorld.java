2983e2fc14fe4cdd9fd0635a4837bab208d68a93
dhj9265 <dhj9265@gmail.com>
2019-11-12 14:30:56 +0900

    어허허허허 유지쿤 수정

--- src/testGithub01/com/kh/testGithub/PrintHelloWorld.java
+++ src/testGithub01/com/kh/testGithub/PrintHelloWorld.java
@@ -5,6 +5,6 @@ public class PrintHelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("YOOZIYOOZIHIHI");
-		    
+		System.out.println("어허어어~~ 유지쿤!");
 	}
 }
